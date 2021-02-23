752a1ac766e710241620f2dad37680ffc5053dab
Thomas Reichmann <thomasreichmann@hotmail.com>
2021-02-23 20:41:34 -0300

    init prototype

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,23 @@ const authProvider = new ClientCredentialsAuthProvider(clientId, clientSecret);
 const apiClient = new ApiClient({ authProvider });
 
 // Nao funciona por erro no nome da categoria
-apiClient.helix.clips.getClipsForGame('League of Legends');
+
+(async () => {
+	try {
+		// let categories = await apiClient.helix.search.searchCategories('league of legends');
+
+		// for (let game of categories.data) {
+		// 	console.log(game.name, game.id);
+		// }
+		let clips = await apiClient.helix.clips.getClipsForGame('21779', {
+			limit: 50,
+			startDate: new Date(Date.now() - 1 * 24 * 60 * 60 * 1000).toISOString(),
+		});
+
+		for (let clip of clips.data) {
+			if (clip.language == 'en') console.log(clip.creationDate, clip.language, clip.title, clip.broadcasterDisplayName, clip.url);
+		}
+	} catch (e) {
+		console.error(e);
+	}
+})();
