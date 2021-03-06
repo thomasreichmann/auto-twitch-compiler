1501d2cd7ff97658c9a30a10b7029763124da1a5
Thomas Reichmann <thomasreichmann@hotmail.com>
2021-03-06 05:31:37 -0300

    beggining to implement normalization on videos

--- src/concat.ts
+++ src/concat.ts
@@ -1,6 +1,11 @@
 import { exec } from 'child_process';
 
+import { normalize } from './normalize';
+
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+
+import util from 'util';
+const awaitExec = util.promisify(exec);
 
 import { promises } from 'fs';
 import { Video } from 'twitch/lib';
@@ -8,21 +13,44 @@ const fs = promises;
 
 let videosFolder = '\\videos\\';
 
+// Consts
+const MAX_BITRATE = 135000;
+
 (async () => {
 	let names = await fs.readdir(__dirname + videosFolder);
 	names = names.filter(s => s.endsWith(`.mp4`));
+	// TODO: change this behabiour, if clipID contains out naturaly, it will be ignored
 	names = names.filter(s => !s.match(/out/));
-	let files = names.map(s => videosFolder + s);
+	let files = names.map(s => `${__dirname}${videosFolder}${s}`);
+
+	// Usamos o normalize.ts para normalizar todos os videos para a mesma bitrate (ajuda a minimizar chances de erro com o concat)
+	let p = [];
+	for (const file of files) {
+		// Usamos o ffprobe para achar todos os videos com audio bitrate > MAX_BITRATE
+		const bitRate = await getAudioBitrate(file);
+
+		console.log(bitRate);
+
+		if (bitRate > MAX_BITRATE) p.push(normalize(file));
+	}
+
+	try {
+		await Promise.all(p);
+	} catch (err) {
+		return console.error(err);
+	}
+
+	// Criamos o arquivo files.txt para o ffmpeg usar como referencia
 	let s = '';
-
-	for (const file of files) s += `file '${__dirname}${file}'\n`;
-
+	for (const file of files) s += `file '${file}'\n`;
 	let filesPath = __dirname + videosFolder + 'files.txt';
 	await fs.writeFile(filesPath, s);
 
 	let outFile = __dirname + videosFolder + 'out.mp4';
 
-	const cmd = '"' + ffmpegPath + '" -y' + ' -f concat' + ' -safe 0' + ' -i "' + filesPath + '"' + ' -c copy "' + outFile + '"';
+	let filer = ' -b:a 128K';
+
+	const cmd = '"' + ffmpegPath + '" -y' + ' -f concat' + ' -safe 0' + ' -i "' + filesPath + '"' + filer + ' -c copy "' + outFile + '"';
 
 	exec(cmd, { maxBuffer: 1024 * 5000 }, (err, stdout, stderr) => {
 		if (err) {
@@ -34,4 +62,10 @@ let videosFolder = '\\videos\\';
 	});
 })();
 
-// TODO: usar o ffmpeg em si na cli, algo parecido com isso https://stackoverflow.com/questions/59232712/how-to-edit-and-concatenation-video
+async function getAudioBitrate(file: String): Promise<number> {
+	const cmd = `ffprobe -v 0 -select_streams a:0 -show_entries stream=bit_rate -of compact=p=0:nk=1 ${file}`;
+	const { stdout, stderr } = await awaitExec(cmd);
+	// Do error checking
+
+	return parseInt(stdout);
+}
--- src/normalize.ts
+++ src/normalize.ts
@@ -1,17 +1,30 @@
-import { exec } from 'child_process';
+import child from 'child_process';
+import util from 'util';
 import { promises } from 'fs';
 const fs = promises;
+const exec = util.promisify(child.exec);
 
-// export async function normalize(files: string[]) {
-// 	for (const file of files) {
-// 	}
-// }
+export async function normalize(file: string) {
+	// TODO: normalize audio sampling rates to 48000K using -ar lower rates than that cause corruption
+	// TODO: normalize fps to 60 using -r lower or higher fps causes corruption
+	// fps: 0.06 lower than 60 causes corruption, but higher doesn't
 
-exec(`ffmpeg -i ${__dirname}\\videos\\corruptor.mp4 -b:a 125K outn.mp4`, (err, stdout, stderr) => {
-	if (err) {
-		console.error(`exec error: ${err}`);
-		return;
+	let newFile = appendToFilename(file, '_fixed');
+	const cmd = `ffmpeg -y -i ${file} -b:a 127K -preset ultrafast ${newFile}`;
+
+	try {
+		const { stdout, stderr } = await exec(cmd);
+
+		// Deletamos o arquivo antigo e mudamos o nome do novo para o do antigo
+		await fs.unlink(file);
+		fs.rename(newFile, file);
+	} catch (err) {
+		throw err;
 	}
+}
 
-	console.log(`process complete`);
-});
+function appendToFilename(filename: string, string: string): string {
+	var dotIndex = filename.lastIndexOf('.');
+	if (dotIndex == -1) return filename + string;
+	else return filename.substring(0, dotIndex) + string + filename.substring(dotIndex);
+}
