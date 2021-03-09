57015dc2c2045eddd2ae7bb36d322664769a5b80
Thomas Reichmann <thomasreichmann@hotmail.com>
2021-03-09 05:30:18 -0300

    normalization more stable

--- src/normalize.ts
+++ src/normalize.ts
@@ -8,9 +8,10 @@ export async function normalize(file: string) {
 	// TODO: normalize audio sampling rates to 48000K using -ar lower rates than that cause corruption
 	// TODO: normalize fps to 60 using -r lower or higher fps causes corruption
 	// fps: 0.06 lower than 60 causes corruption, but higher doesn't
+	// fps: 30.02fps does not corrupt for some reason????????? but 50.08 does?????????
 
 	let newFile = appendToFilename(file, '_fixed');
-	const cmd = `ffmpeg -y -i ${file} -b:a 127K -preset ultrafast ${newFile}`;
+	const cmd = `ffmpeg -y -i ${file} -b:a 127K -ar 48000 -r 60 -preset ultrafast ${newFile}`;
 
 	try {
 		const { stdout, stderr } = await exec(cmd);
