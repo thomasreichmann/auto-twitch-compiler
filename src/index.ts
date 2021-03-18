802be821bcbe5b6b4291c89551d667cd3ea51223
Thomas Reichmann <thomasreichmann@hotmail.com>
2021-03-17 23:01:50 -0300

    moved startDate to const

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,15 @@ import { concat } from './concat';
 import { fetchVideos } from './fetchVideos';
 
 const videosDir = __dirname + '\\videos\\';
+// Uma data representando qual o limite de tempo para buscar clipes
+const maxVideoAge = new Date(Date.now() - 1 * 24 * 60 * 60 * 1000).toISOString();
 
 (async () => {
 	try {
 		fs.rmdirSync(videosDir, { recursive: true });
 		fs.mkdirSync(videosDir);
 
-		await fetchVideos(videosDir, '21779', 100, new Date(Date.now() - 1 * 24 * 60 * 60 * 1000).toISOString());
+		await fetchVideos(videosDir, '21779', 100, maxVideoAge);
 		await concat(videosDir);
 	} catch (err) {
 		console.error(err);
