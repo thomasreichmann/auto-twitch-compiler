cc2188d6f93cc837b0178464f2136d680a3e5718
Thomas Reichmann <thomasreichmann@hotmail.com>
2021-04-07 01:51:40 -0300

    tasks array was being reset on each channel now the new tasks are concatnated to the array

--- src/setUploadTimeouts.ts
+++ src/setUploadTimeouts.ts
@@ -1,5 +1,5 @@
 import Channel from './interfaces/Channel.interface';
-import cron, { schedule } from 'node-cron';
+import cron from 'node-cron';
 
 /**
  * Funcao leva um array de canais, cria setTimeouts para cada horario de upload
@@ -11,15 +11,18 @@ function setUploadTimeouts(channels: Channel[], upload: (c: Channel) => any) {
 	// Create an array to hold all of the tasks
 	let tasks: cron.ScheduledTask[] = [];
 	for (const channel of channels) {
-		// Go through the upload times and create a task for each one
-		tasks = channel.uploadTimes.map(time => {
-			// Do cron scheduling with the time provided in format HH:MM
-			let [hours, minutes] = time.split(':');
-			let expression = `${minutes} ${hours} * * *`;
-			if (!cron.validate(expression)) throw new Error(`Invalid uploadTime for channel ${channel.id} with time ${time}`);
+		// Add this channel's array of tasks to the general array
+		tasks = tasks.concat(
+			// Go through the upload times and create a task for each one
+			channel.uploadTimes.map(time => {
+				// Do cron scheduling with the time provided in format HH:MM
+				let [hours, minutes] = time.split(':');
+				let expression = `${minutes} ${hours} * * *`;
+				if (!cron.validate(expression)) throw new Error(`Invalid uploadTime for channel ${channel.id} with time ${time}`);
 
-			return cron.schedule(expression, () => upload(channel));
-		});
+				return cron.schedule(expression, () => upload(channel));
+			})
+		);
 	}
 	return tasks;
 }
